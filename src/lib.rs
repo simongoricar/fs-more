ca0950c3822e02848630eea2544e1e2ab8f3b296
Simon Goričar <simon.peter.goricar@gmail.com>
2024-05-13 14:06:21 +0200

    fix: fix outdated doctests

--- src/lib.rs
+++ src/lib.rs
@@ -31,15 +31,20 @@
 //! ```no_run
 //! # use std::path::Path;
 //! # use fs_more::error::FileError;
-//! # use fs_more::file::FileCopyWithProgressOptions;
+//! # use fs_more::file::CopyFileWithProgressOptions;
+//! # use fs_more::file::CopyFileFinished;
+//! # use fs_more::file::ExistingFileBehaviour;
 //! # fn main() -> Result<(), FileError> {
 //! let source_path = Path::new("./source-file.txt");
 //! let destination_path = Path::new("./target-file.txt");
 //!
-//! let bytes_copied = fs_more::file::copy_file_with_progress(
+//! let finished_copy = fs_more::file::copy_file_with_progress(
 //!     source_path,
 //!     destination_path,
-//!     FileCopyWithProgressOptions::default(),
+//!     CopyFileWithProgressOptions {
+//!         existing_destination_file_behaviour: ExistingFileBehaviour::Abort,
+//!         ..Default::default()
+//!     },
 //!     |progress| {
 //!         let percent_copied =
 //!             (progress.bytes_finished as f64) / (progress.bytes_total as f64)
@@ -49,7 +54,17 @@
 //!     }
 //! )?;
 //!
-//! println!("Copied {bytes_copied} bytes!");
+//! match finished_copy {
+//!     CopyFileFinished::Created { bytes_copied } => {
+//!         println!("Copied {bytes_copied} bytes!");
+//!     }
+//!     CopyFileFinished::Overwritten { bytes_copied } => {
+//!         println!("Copied {bytes_copied} bytes over an existing file!");
+//!     }
+//!     // ...
+//!     _ => {}
+//! };
+//!
 //! # Ok(())
 //! # }
 //! ```
@@ -57,17 +72,17 @@
 //! Moving a directory and getting updates on the progress:
 //! ```no_run
 //! # use std::path::Path;
-//! # use fs_more::error::DirectoryError;
-//! # use fs_more::directory::DirectoryMoveWithProgressOptions;
+//! # use fs_more::error::MoveDirectoryError;
+//! # use fs_more::directory::MoveDirectoryWithProgressOptions;
 //! # use fs_more::directory::DestinationDirectoryRule;
-//! # fn main() -> Result<(), DirectoryError> {
+//! # fn main() -> Result<(), MoveDirectoryError> {
 //! let source_path = Path::new("./source-directory");
 //! let destination_path = Path::new("./target-directory");
 //!
 //! let moved = fs_more::directory::move_directory_with_progress(
 //!     source_path,
 //!     destination_path,
-//!     DirectoryMoveWithProgressOptions {
+//!     MoveDirectoryWithProgressOptions {
 //!         destination_directory_rule: DestinationDirectoryRule::AllowEmpty,
 //!         ..Default::default()
 //!     },
@@ -88,9 +103,9 @@
 //! println!(
 //!     "Moved {} bytes ({} files, {} directories)! Underlying strategy: {:?}.",
 //!     moved.total_bytes_moved,
-//!     moved.num_files_moved,
-//!     moved.num_directories_moved,
-//!     moved.used_strategy
+//!     moved.files_moved,
+//!     moved.directories_moved,
+//!     moved.strategy_used
 //! );
 //! # Ok(())
 //! # }
