0a3872f9f24af4a885d217f50aa8f14c5cca6683
Simon Goričar <simon.peter.goricar@gmail.com>
2024-07-10 14:15:04 +0200

    test: add tests to check basic `is_directory_empty` functionality

---
+++ tests/integration/directory/is_empty.rs
@@ -0,0 +1,23 @@
+use fs_more_test_harness::{
+    assertable::AsPath,
+    tree_framework::FileSystemHarness,
+    trees::{empty::EmptyTree, simple::SimpleTree},
+};
+
+#[test]
+pub fn is_directory_empty_reports_true_on_empty_directory() {
+    let empty_harness = EmptyTree::initialize();
+
+    let is_empty = fs_more::directory::is_directory_empty(empty_harness.as_path()).unwrap();
+
+    assert!(is_empty);
+}
+
+#[test]
+pub fn is_directory_empty_reports_false_on_non_empty_directory() {
+    let simple_harness = SimpleTree::initialize();
+
+    let is_empty = fs_more::directory::is_directory_empty(simple_harness.as_path()).unwrap();
+
+    assert!(!is_empty);
+}
